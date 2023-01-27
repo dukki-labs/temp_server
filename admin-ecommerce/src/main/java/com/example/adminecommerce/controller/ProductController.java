c7d5d5d26955efa85d98f0b1f069722c99d6709d
phantom08266 <phantom08266@gmail.com>
2023-01-28 00:14:51 +0900

    테스트 프로젝트

---
+++ admin-ecommerce/src/main/java/com/example/adminecommerce/controller/ProductController.java
@@ -0,0 +1,13 @@
+package com.example.adminecommerce.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProductController {
+
+    @GetMapping("/products")
+    public String products() {
+        return "/products/product";
+    }
+}
