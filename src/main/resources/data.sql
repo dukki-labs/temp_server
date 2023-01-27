260047623735da7ace13a5e54487c67a4d9dceb6
phantom08266 <phantom08266@gmail.com>
2023-01-28 00:48:04 +0900

    파일위치 변경

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO customers (name, age, phoneNumber, address, grade, isDeleted, createdAt, updatedAt)
+VALUES ( '홍길동', 13 , '010-0000-0000', 'testtsetet', 'BASIC',false, '2023-01-27 09:56:49.774164', '2023-01-27 09:56:49.774164');
+
+
+INSERT INTO customers (name, age, phoneNumber, address, grade, isDeleted, createdAt, updatedAt)
+VALUES ( '홍길동2', 20 , '010-1111-0000', 'testtsetet', 'BASIC',false, '2023-01-27 09:56:49.774164', '2023-01-27 09:56:49.774164');
+
+INSERT INTO customers (name, age, phoneNumber, address, grade, isDeleted, createdAt, updatedAt)
+VALUES ( '홍길동3', 30 , '010-3333-0000', 'testtsetet', 'BASIC',false, '2023-01-27 09:56:49.774164', '2023-01-27 09:56:49.774164');
+
+INSERT INTO customers (name, age, phoneNumber, address, grade, isDeleted, createdAt, updatedAt)
+VALUES ( '홍길동4', 24 , '010-2424-0000', 'testtsetet', 'BASIC',false, '2023-01-27 09:56:49.774164', '2023-01-27 09:56:49.774164');
